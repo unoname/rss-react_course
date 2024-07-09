a2207bbdfcfbcfebfd7b2a41008b4a03c55a82ab
aleks_pv <apanov08@gmail.com>
2024-07-09 19:50:21 +0400

    chore: change rule and add setting in eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/jsx-runtime',
     'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
@@ -17,13 +18,27 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    "react-compiler/react-compiler": "error",
+    "react-compiler/react-compiler": 'error',
     '@typescript-eslint/no-explicit-any': 'error',
+    'react/react-in-jsx-scope': 'off',
+    "@typescript-eslint/no-misused-promises": ['error', {
+    "checksVoidReturn": {
+      "attributes": false
+    }
+  }]
   },
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    
-  }
+    project: ['./tsconfig.json', './tsconfig.app.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+     version: "detect",
+    },
+  },
 }
